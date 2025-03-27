cb1fbf1b1ee91a593d2947801c93d991b4e32cbe
senorkumar <sdkumar@hmc.edu>
2025-03-27 10:46:51 -0700

    setting up infra

---
+++ src/box_orientation/cameras.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+
+def get_views() -> dict[str, Image.Image]:
+    """
+    Returns a dictionary of views from the cameras:
+    {
+        "top": Image.Image,
+        "front": Image.Image,
+        "side": Image.Image,
+    }
+    """
+    pass
---
+++ src/box_orientation/orientation.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+
+# loads Groundlight API key from .env file
+load_dotenv()
